d39b300a9d7773d0032add24f966f0ac66d932ed
Mark Bouchard <mark_bouchard@hotmail.com>
2015-11-04 15:37:49 -0500

    options page saving and loading properly

--- options.js
+++ options.js
@@ -1,47 +1,61 @@
 
 function save_options() {
 
+	// get the input form values
+	var primary_interval = document.getElementById('primary_interval').value;
+	var primary_count = document.getElementById('primary_count').value;
+	var secondary_interval = document.getElementById('secondary_interval').value;
+	var autoStartYes = document.getElementById('autoStartYes').checked;
+	
+	chrome.extension.getBackgroundPage().console.log('inside options.js save_options');
+	chrome.extension.getBackgroundPage().console.log('primary_interval = ' + primary_interval);
+	chrome.extension.getBackgroundPage().console.log('primary_count = ' + primary_count);
+	chrome.extension.getBackgroundPage().console.log('secondary_interval = ' + secondary_interval);
+	chrome.extension.getBackgroundPage().console.log('autoStartYes = ' + autoStartYes);
 
-    alert("hello save");
-    chrome.extension.getBackgroundPage().console.log('inside options.js saveOptions');
+	localStorage.setItem("infooven_primary_interval", primary_interval);
+	localStorage.setItem("infooven_primary_count", primary_count);
+	localStorage.setItem("infooven_secondary_interval", secondary_interval);
+	localStorage.setItem("infooven_auto_start", autoStartYes);
 
-/*
-    var color = document.getElementById('color').value;
-	var likesColor = document.getElementById('like').checked;
-	chrome.storage.sync.set({
-	favoriteColor: color,
-	likesColor: likesColor
-	}, function() {
-	// Update status to let user know options were saved.
-	var status = document.getElementById('status');
-	status.textContent = 'Options saved.';
-	setTimeout(function() {
-	  status.textContent = '';
-	}, 750);
-	});
-*/
-    /*
-    var select = document.getElementById("color");
-    var color = select.children[select.selectedIndex].value;
-    localStorage["favColor"] = color;
-    */
+	chrome.extension.getBackgroundPage().console.log('save_options - auto_start = ' +autoStartYes);
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
 
-  //alert("inside restore_options");
-  /*
-  chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
-  }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
-  });
-*/
+	chrome.extension.getBackgroundPage().console.log('restore_options loaded');
+
+	// get the ol' items from storage	
+	var primary_interval = localStorage.getItem("infooven_primary_interval") || 1.0;
+    var primary_count = localStorage.getItem("infooven_primary_count") || 1;
+    var secondary_interval = localStorage.getItem("infooven_secondary_interval") || 0.05;
+    var auto_start = localStorage.getItem("infooven_auto_start");
+
+    // validate auto_start boolean
+    if(auto_start != null)
+    {
+    	auto_start = JSON.parse(auto_start);
+
+	}
+	else
+	{
+		// default to true
+		auto_start = true;
+	}
+
+	// set the items in the page 
+	document.getElementById('primary_interval').value = primary_interval;
+	document.getElementById('primary_count').value = primary_count;
+	document.getElementById('secondary_interval').value = secondary_interval;
+
+	// default the radio buttons
+	document.getElementById('autoStartYes').checked = auto_start;	
+	document.getElementById('autoStartNo').checked = !auto_start;		
+
+  	chrome.extension.getBackgroundPage().console.log('done with restore_options');
+
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
